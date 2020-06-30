fee1b9e0465d72390daa590e755a7dd80dbe56e3
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-06-30 12:01:41 +0100

    create schema

---
+++ studio/schemas/documents/slide.js
@@ -0,0 +1,17 @@
+export default {
+  name: "slide",
+  type: "document",
+  title: "Slide",
+  fields: [
+    {
+      name: "image",
+      type: "slideImage",
+      title: "Image",
+    },
+    {
+      name: "caption",
+      type: "string",
+      title: "Image Caption",
+    },
+  ],
+};
