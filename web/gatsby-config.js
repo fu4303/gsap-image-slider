cc58452e94988050f37e05e0bf2ae0173dc725a9
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-06-30 10:18:40 +0100

    initial commit

---
+++ web/gatsby-config.js
@@ -0,0 +1,20 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV || "development"}`,
+})
+
+const isProd = process.env.NODE_ENV === "production"
+
+module.exports = {
+  plugins: [
+    // {
+    //   resolve: "gatsby-source-sanity",
+    //   options: {
+    //     projectId: "bxkw0htn",
+    //     dataset: "production",
+    //     // token: process.env.SANITY_TOKEN,
+    //     watchMode: !isProd,
+    //     overlayDrafts: !isProd,
+    //   },
+    // },
+  ],
+}
