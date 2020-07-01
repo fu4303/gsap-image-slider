603bb881f869569ba669fa7a85894f3c1b64f588
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-07-01 09:32:04 +0100

    create slider

--- web/src/components/Slider.js
+++ web/src/components/Slider.js
@@ -1,11 +1,54 @@
-import React from "react"
+import React, { useEffect, useState, useRef } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 import { gsap, Draggable } from "gsap/all"
+import styles from "./slider.module.css"
+import Arrow from "./Arrow"
 
 gsap.registerPlugin(Draggable)
 
 const Slider = () => {
+  const [activeSlide, setActiveSlide] = useState(0)
+  const [dimensions, setDimensions] = useState({
+    width: 590,
+  })
+  let outerRef = useRef(null)
+  let sliderRef = useRef(null)
+
+  useEffect(() => {
+    gsap.to(sliderRef.current, {
+      duration: 0.7,
+      x: -(dimensions.width * activeSlide),
+    })
+  }, [activeSlide, dimensions])
+
+  useEffect(() => {
+    setDimensions({
+      width: outerRef.current.clientWidth,
+    })
+  }, [])
+
+  useEffect(() => {
+    function handleResize() {
+      setDimensions({
+        width: outerRef.current.clientWidth,
+      })
+    }
+
+    window.addEventListener("resize", handleResize)
+    return (_) => {
+      window.removeEventListener("resize", handleResize)
+    }
+  })
+
+  const handleGoToPrevious = () => {
+    setActiveSlide(activeSlide - 1)
+  }
+
+  const handleGoToNext = () => {
+    setActiveSlide(activeSlide + 1)
+  }
+
   const data = useStaticQuery(graphql`
     {
       allSanitySlide {
@@ -24,12 +67,42 @@ const Slider = () => {
       }
     }
   `)
+
+  const numberOfSlides = data.allSanitySlide.edges.length
+
   return (
-    <div>
-      {data.allSanitySlide.edges.map(({ node: slide }, i) => (
-        <Img fluid={slide.image.asset.fluid} />
-      ))}
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+    <div className={styles.container}>
+      <div className={styles.outer} ref={outerRef}>
+        <div
+          style={{ width: `${dimensions.width * numberOfSlides}px` }}
+          className={styles.inner}
+          ref={sliderRef}
+        >
+          {data.allSanitySlide.edges.map(({ node: slide }, i) => (
+            <Img
+              key={i}
+              fluid={slide.image.asset.fluid}
+              style={{ width: `${dimensions.width}px` }}
+            />
+          ))}
+        </div>
+        <div className={styles.controlContainer}>
+          <button
+            className={`${styles.controlButton} ${styles.controlButtonLeft}`}
+            onClick={handleGoToPrevious}
+            disabled={activeSlide === 0}
+          >
+            <Arrow />
+          </button>
+          <button
+            className={styles.controlButton}
+            onClick={handleGoToNext}
+            disabled={activeSlide === 3}
+          >
+            <Arrow />
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
