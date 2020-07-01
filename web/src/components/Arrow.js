51c1814efc66a4c716faa861be731db0292f97d1
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-07-01 09:35:08 +0100

    create arrow component

---
+++ web/src/components/Arrow.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Arrow = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 15.8 6.8"
+      width="20"
+      height="20"
+    >
+      <path d="M11.6 0l-.7.8 2.7 2.1H0v1h13.6L10.9 6l.7.8 4.2-3.4z" />
+    </svg>
+  )
+}
+
+export default Arrow
