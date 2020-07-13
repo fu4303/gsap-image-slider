d536b5692d1b1cfa3d2e24c0ee664124d40e5c7e
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-07-13 11:59:12 +0100

    change style of dots

--- web/src/components/Dot.js
+++ web/src/components/Dot.js
@@ -4,21 +4,34 @@ import styles from "./dot.module.css"
 const Dot = ({ slide, activeSlide, setActiveSlide }) => {
   return (
     <button
-      className={`${styles.dot} ${activeSlide === slide ? styles.space : ""} ${
-        activeSlide >= slide ? styles.white : ""
-      }`}
+      className={styles.dot}
       aria-expanded="false"
       onClick={() => setActiveSlide(slide)}
     >
-      <svg
-        viewBox="0 0 5 5"
-        width="5"
-        height="5"
-        aria-hidden="true"
-        focusable="false"
-      >
-        <circle cx="2.5" cy="2.5" r="2.5" />
-      </svg>
+      {activeSlide === slide ? (
+        <svg
+          width="10"
+          height="10"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M9.25 5a4.25 4.25 0 11-8.5 0 4.25 4.25 0 018.5 0z"
+            stroke="#ffffff"
+            stroke-width="1.5"
+          />
+        </svg>
+      ) : (
+        <svg
+          width="10"
+          height="10"
+          viewBox="0 0 10 10"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M5 10C7.76142 10 10 7.76142 10 5C10 2.23858 7.76142 0 5 0C2.23858 0 0 2.23858 0 5C0 7.76142 2.23858 10 5 10Z" />
+        </svg>
+      )}
       <span hidden className="srOnly">
         {`Go to slide ${slide}`}
       </span>
